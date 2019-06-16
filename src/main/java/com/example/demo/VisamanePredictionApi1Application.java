82e6916b07f518ae3198f67c00fbe441eab7ed89
djo <nhadjmbarek@hrdatabank.com>
2019-06-17 08:25:48 +0900

    first commit

---
+++ src/main/java/com/example/demo/VisamanePredictionApi1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisamanePredictionApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisamanePredictionApi1Application.class, args);
+	}
+
+}
