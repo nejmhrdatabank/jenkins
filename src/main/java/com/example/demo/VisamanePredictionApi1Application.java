6e0d70cb48703da5e8bc28bdf9bab58a614e37be
djo <nhadjmbarek@hrdatabank.com>
2019-06-20 19:10:21 +0900

    commit test webhhokk

--- src/main/java/com/example/demo/VisamanePredictionApi1Application.java
+++ src/main/java/com/example/demo/VisamanePredictionApi1Application.java
@@ -9,7 +9,7 @@ public class VisamanePredictionApi1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(VisamanePredictionApi1Application.class, args);
 		
-		System.out.println("*:**********test webhook*****");
+		System.out.println("*:**********test webhook 3*****");
 	}
 
 }
